490735d2f01b1c5f04665e24616811a67ab3a2ea
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2021-08-24 19:28:25 +0600

    Add files via upload

---
+++ Hackerrank/Data Structures/Linked Lists/Insert a node at the head of a linked list.py
@@ -0,0 +1,44 @@
+def insertNodeAtHead(llist, data):
+    x = SinglyLinkedListNode(data)
+    if (llist == None):
+        llist = x
+    else:
+        new_node = x
+        new_node.next = llist
+        llist = new_node
+    return llist
+
+"""class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(self):
+    temp = self.head
+    while temp is not None:
+        print(temp.data, end=" ")
+        temp = temp.next
+
+def insertNodeAtHead(llist, data):
+    x = SinglyLinkedListNode(data)
+    if (llist == None):
+        llist = x
+    else:
+        new_node = x
+        new_node.next = llist
+        llist = new_node
+    return llist
+
+if __name__ == '__main__':
+    llist_count = int(input())
+    llist = SinglyLinkedList()
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtHead(llist.head, llist_item)
+        llist.head = llist_head
+    print_singly_linked_list(llist)"""
