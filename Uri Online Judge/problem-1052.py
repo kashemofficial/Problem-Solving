b0707f73cd02b25a5403e51394c8d6cede9ca884
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2021-10-13 10:14:48 +0600

    Add files via upload

---
+++ Uri Online Judge/problem-1052.py
@@ -0,0 +1,53 @@
+n = int(input())
+if n == 1:
+    print('January')
+elif n == 2:
+    print('February')
+elif n == 3:
+    print('March')
+elif n == 4:
+    print('April')
+elif n == 5:
+    print('May')
+elif n == 6:
+    print('June')
+elif n == 7:
+    print('July')
+elif n == 8:
+    print('August')
+elif n == 9:
+    print('September')
+elif n == 10:
+    print('October')
+elif n == 11:
+    print('November')
+elif n == 12:
+    print('December')
+
+'''li = ['January','February','March','April','May','June','July','August','September','October','November','December']
+n = int(input())
+if n == 1:
+    print(li[0])
+elif n == 2:
+    print(li[1])
+elif n == 3:
+    print(li[2])
+elif n == 4:
+    print(li[3])
+elif n == 5:
+    print(li[4])
+elif n == 6:
+    print(li[5])
+elif n == 7:
+    print(li[6])
+elif n == 8:
+    print(li[7])
+elif n == 9:
+    print(li[8])
+elif n == 10:
+    print(li[9])
+elif n == 11:
+    print(li[10])
+elif n == 12:
+    print(li[11])'''
+
