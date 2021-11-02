48dc25d017edb08eed3dce72c518d16460b7b004
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2021-11-03 00:14:05 +0600

    Add files via upload

---
+++ Codeforces/(122 A) Lucky Division.py
@@ -0,0 +1,31 @@
+n = int(input())
+if (n%4==0):
+    print('YES')
+elif(n%7==0):
+    print('YES')
+elif (n%44==0):
+    print('YES')
+elif (n%47==0):
+    print('YES')
+elif (n%74==0):
+    print('YES')
+elif (n%77==0):
+    print('YES')
+elif (n%444==0):
+    print('YES')
+elif (n%447==0):
+    print('YES')
+elif (n%474==0):
+    print('YES')
+elif (n%477==0):
+    print('YES')
+elif (n%744==0):
+    print("YES")
+elif (n%747==0):
+    print('YES')
+elif (n%774==0):
+    print('YES')
+elif (n%777==0):
+    print('YES')
+else:
+    print('NO')
