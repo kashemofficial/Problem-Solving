0dcc670b9b3a8ce68415ff27890663fd20e6f427
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2021-11-04 15:29:58 +0600

    Add files via upload

---
+++ (96 A) Football.py
@@ -0,0 +1,16 @@
+n = input()
+if '0'*7 in n or '1'*7 in n:
+    print('YES')
+else:
+    print('NO')
+
+
+'''a = '0000000'
+b = '1111111'
+n = input()
+if a in n or b in n:
+    print('YES')
+else:
+    print('NO')'''
+
+
