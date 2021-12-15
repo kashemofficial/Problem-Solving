7ae797d1a3d238d5751d7b722a9d273f74a6a722
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2021-12-15 01:47:50 -0800

    Rename main.py to main1.py

--- (1602 A) Two Subsequences.py
+++
@@ -1,9 +0,0 @@
-def solve(n):
-    m = min(n)
-    result = n.replace(m,"",1)
-    print(m,result)
-if __name__ == '__main__':
-    for _ in range(int(input())):
-        n = input()
-        solve(n)
-
