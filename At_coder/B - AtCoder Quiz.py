00f8acd41e41b0c93b2af52c0f17bb4e1d42132f
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2022-01-25 21:59:13 +0600

    Add files via upload

---
+++ At_coder/B - AtCoder Quiz.py
@@ -0,0 +1,6 @@
+arr = ['ABC','ARC', 'AGC','AHC']
+for i in range(3):
+    s = input()
+    arr.remove(s)
+print(arr[0])
+
---
+++ At_coder/B - String Shifting.py
@@ -0,0 +1,5 @@
+s = list(input())
+res = sorted(s)
+res1 = res[::-1]
+print(*res,sep='')
+print(*res1,sep='')
