166025bcd6ccbf59cd6979ae06c6063a7084d753
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2022-01-27 15:31:03 +0600

    Add files via upload

---
+++ Codechef/First and Last Digit.py
@@ -0,0 +1,4 @@
+for _ in range(int(input())):
+    n = input()
+    res = int(n[0])+int(n[-1])
+    print(res)
---
+++ Codechef/Sum of Digits.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    n = input()
+    c = []
+    for i in n:
+        c.append(int(i))
+    print(sum(c))
