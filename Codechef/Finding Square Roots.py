5752478e10c8c40689a7fd8b5f5b767469985f45
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2022-01-28 22:06:06 +0600

    Add files via upload

---
+++ Codechef/Finding Square Roots.py
@@ -0,0 +1,6 @@
+for t in range(int(input())):
+    import math
+    a = int(input())
+    result = math.sqrt(a)
+    print(int(result))
+
---
+++ Codechef/Small Factorial.py
@@ -0,0 +1,4 @@
+import math
+for t in range(int(input())):
+    n = int(input())
+    print(math.factorial(n))
