02e7a07f52c69ceb349c942de08bcf3672dd2371
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2022-02-03 21:23:20 +0600

    Add files via upload

---
+++ Codeforces/(1196 A) Three Piles of Candies.py
@@ -0,0 +1,4 @@
+for _ in range(int(input())):
+    a,b,c = map(int,input().split())
+    print((a+b+c)//2)
+
---
+++ Codeforces/(1527 A) And Then There Were K.py
@@ -0,0 +1,7 @@
+import math
+for _ in range(int(input())):
+    n = int(input())
+    print(2**int(math.log(n,2))-1)
+
+
+
