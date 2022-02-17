526d455dad294088f4eb697a7591f39147b9487a
MD Abul Kashem <78655785+kashemofficial@users.noreply.github.com>
2022-02-17 19:42:39 +0600

    Add files via upload

---
+++ Codeforces/(1605 A) A.M. Deviation.py
@@ -0,0 +1,22 @@
+for t in range(int(input())):
+    a1,a2,a3 = map(int,input().split())
+    ans = (a1+a3-2*a2)
+    if ans%2 == 0:
+        print(0)
+    else:
+        print(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Codeforces/(1608 A) Find Array.py
@@ -0,0 +1,8 @@
+for t in range(int(input())):
+    a=int(input())
+    b=[]
+    for i in range(2,a+2):
+        b.append(i)
+    print(*b)
+
+
